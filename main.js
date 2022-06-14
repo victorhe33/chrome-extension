8937702e75f2177ad5fc1f7e577313b002af7217
Victor <94798862+victorhe33@users.noreply.github.com>
2022-06-14 18:17:54 -0400

    added some flexbox styling, test handlers/async

--- main.js
+++ main.js
@@ -5,25 +5,31 @@
 const primary = document.getElementById('primary');
 const parent = primary.parentNode;
 
+//Create Holding container, set parent class attribute for styling purposes
+const newContainer = document.createElement('div');
+newContainer.className = 'container';
+parent.insertBefore(newContainer, primary);
+
 //REMOVE OFFENDING CONTENT
 primary.remove();
-
-//We chose to just inject a style sheet using our chrome extension manifest.
-// const link = document.createElement('link');
-// link.rel = 'stylesheet';
-// link.type = 'text/css';
-// link.href = chrome.extension.getURL('style.css');
-// document.head.prepend(link);
 
 //REPLACE WITH IMAGE
 const image = document.createElement('img');
 image.setAttribute('src', 'https://www.motorbiscuit.com/wp-content/uploads/2022/04/1994-Mazda-Miata.jpg');
 image.setAttribute('class', 'image');
 image.setAttribute('id', 'image');
-parent.prepend(image);
+// newContainer.prepend(image);
 
 //Add text div
 const text = document.createElement('div');
-text.class = 'text';
+text.className = 'text';
 text.innerText = "Get Miata Here!";
-parent.appendChild(text);
+// newContainer.appendChild(text);
+
+
+setTimeout(function() {
+  newContainer.prepend(image);
+  newContainer.appendChild(text);
+}, 3000);
+
+newContainer.addEventListener('click', newContainer.remove);
--- main2.js
+++ main2.js
@@ -4,23 +4,28 @@
 
 const primary = document.getElementById('pageContent');
 const rhf = document.getElementById('rhf');
-const newContainer = document.createElement('div');
 const parent = primary.parentNode;
-parent.insertBefore(newContainer, primary);
-// console.log('hello', parent);
 
+//SET CONTAINER CLASS FOR STYLING & insert
+const newContainer = document.createElement('div');
+newContainer.className = 'container';
+parent.insertBefore(newContainer, primary);
+
+//REMOVE OFFENDING ELEMENTS
 primary.remove();
 rhf.remove();
+
+
 
 //REPLACE WITH IMAGE
 const image = document.createElement('img');
 image.setAttribute('src', 'https://www.motorbiscuit.com/wp-content/uploads/2022/04/1994-Mazda-Miata.jpg');
-image.setAttribute('style', 'width: 400px;');
+image.className = 'image';
 image.setAttribute('id', 'image');
 newContainer.prepend(image);
 
 //Add text div
 const text = document.createElement('div');
-text.class = 'text';
+text.className = 'text';
 text.innerText = "Get Miata Here!";
 newContainer.appendChild(text);
